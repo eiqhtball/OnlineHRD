a4d73f874ca314324179fb571f9c5c7d3c731f5d
Muhammad Iqbal Tawakkal <muhammad.tawakkal@students.amikom.ac.id>
2017-10-16 22:39:15 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,13 +7,16 @@ if (isset($_POST['submit'])) {
 	// exit;
 
 	$fullname = $_POST['fullname'];
+	$noktp = $_POST['ktp'];
 	$username = $_POST['username'];
 	$email = $_POST['email'];
+	$gender = $_POST['gender'];
 	$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
 
+	$result = $conn->query("INSERT INTO t_user (userID, noktp,fullname, username, email, gender, password) 
+		VALUES (null, '$noktp','$fullname','$username','$email','$gender','$password')");
 
-	$result = $conn->query("INSERT INTO t_user (userID,fullname, username, email, password) VALUES (null,'$fullname','$username','$email','$password')");
 	
 	if ($result) {
 		echo '<script>alert("pendaftaran sukses!")</script>';
@@ -51,37 +54,83 @@ CloseCon($conn);
 				</div>
 			</div> -->
 
-			<div class="container" style="margin-top: 60px;">
+			<div class="container" style="margin-top: 20px;">
 				<div class="row">
-				  	<div class="col-md-7">
-				  		
+				  	<div class="col-md-8" style="margin-top: 20px;">
+				  		<img src="Assets/img/brand.png" style="width: 50%; height: 50%;"><br><br>
+				  		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+						  <!-- Indicators -->
+						  <ol class="carousel-indicators">
+						    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						    <li data-target="#myCarousel" data-slide-to="1"></li>
+						    <li data-target="#myCarousel" data-slide-to="2"></li>
+						  </ol>
+
+						  <!-- Wrapper for slides -->
+						  <div class="carousel-inner">
+						    <div class="item active">
+						      <img class="gambarslide" src="Assets/img/1.jpg" alt="Los Angeles">
+						    </div>
+
+						    <div class="item">
+						      <img class="gambarslide" src="Assets/img/2.jpg" alt="Chicago">
+						    </div>
+
+						    <div class="item">
+						      <img class="gambarslide" src="Assets/img/2.jpg" alt="New York">
+						    </div>
+						  </div>
+
+						  <!-- Left and right controls -->
+						  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+						    <span class="glyphicon glyphicon-chevron-left"></span>
+						    <span class="sr-only">Previous</span>
+						  </a>
+						  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+						    <span class="glyphicon glyphicon-chevron-right"></span>
+						    <span class="sr-only">Next</span>
+						  </a>
+						</div>
 				  	</div>
 					  
-					<div class="col-md-5" style="padding: 0px;">
-						<div class="banner">
-							<p class="bannerregis" >
+					<div class="col-md-4" style="padding: 0px;">
+						<div class="banner" style="border-radius: 10px; background-color: #E1FAFC; ">
+							<p class="bannerregis" style="border-radius: 10px;" >
 							Sign Up Here! <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
 							</p>
 						</div>
-						  	
-						<form class="formregis" style="background-color: #bdc3c7;" method="post">
-							<div class="form-group">
-								<label for="name">Nama Lengkap:</label>
+
+						<form class="formregis" style="background-color: #EEEEEE; border-radius: 10px;" method="post">
+							<div class="form-group" style="margin-bottom: 0px;">
+								<label>No.KTP:</label>
+								<input type="text" class="form-control" id="noktp" name="ktp">
+							</div>
+							<div class="form-group" style="margin-bottom: 0px;">
+								<label>Nama Lengkap:</label>
 								<input type="name" class="form-control" id="name" name="fullname">
 							</div>
-							<div class="form-group">
-								<label for="username">Username:</label>
+							<div class="form-group" style="margin-bottom: 0px;">
+								<label>Username:</label>
 								<input type="username" class="form-control" id="username" name="username">
 							</div>
-							<div class="form-group">
-								<label for="email">E-mail:</label>
+							<div class="form-group" style="margin-bottom: 0px;">
+								<label>E-mail:</label>
 								<input type="email" class="form-control" id="email" name="email">
 							</div>
-							<div class="form-group">
-								<label for="pwd">Password:</label>
+							<div class="form-group" name="gender" style="margin-bottom: 0px;" >
+								<label>Gender</label><br>
+							  	<input type="radio" name="gender" value="Male"><label>Male</label></input>
+							  	<input type="radio" name="gender" value="Female"><label>Female</label><br></input>
+							</div>
+							<div class="form-group" style="margin-bottom: 0px;" >
+								<label>Password:</label>
 								<input type="password" class="form-control" id="pwd" name="password">
 							</div>
-							<button type="submit" class="btn btn-default" name="submit">Sign up</button>
+							<div class="form-group">
+								<label>Re-Password:</label>
+								<input type="password" class="form-control" id="re-pwd" name="re-password">
+							</div>
+							<button type="submit" class="btn btn-primary" name="submit">Sign up</button>
 						</form>
 							
 						</div>
--- t_user.sql
+++ t_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 16 Okt 2017 pada 05.51
+-- Generation Time: 16 Okt 2017 pada 17.33
 -- Versi Server: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -30,9 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `t_user` (
   `userID` int(9) NOT NULL,
+  `noktp` text NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `username` varchar(40) NOT NULL,
   `email` varchar(40) NOT NULL,
+  `gender` text NOT NULL,
   `password` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -40,11 +42,25 @@ CREATE TABLE `t_user` (
 -- Dumping data untuk tabel `t_user`
 --
 
-INSERT INTO `t_user` (`userID`, `fullname`, `username`, `email`, `password`) VALUES
-(1, 'lalala', 'lala', 'lala@ail.com', 'lala'),
-(2, 'lalala', 'lala', 'lala@ail.com', 'lala'),
-(3, 'asdas', 'asdas', 'asdas@gmail.asdasd', 'asdasdasd'),
-(4, 'qweqweqw', 'qweqweqwe', 'qweqweqwe@asdasd', '$2y$10$fF1jMoUTY5f6mnp2riiwvOvrM3y6AXRou');
+INSERT INTO `t_user` (`userID`, `noktp`, `fullname`, `username`, `email`, `gender`, `password`) VALUES
+(1, '0', 'lalala', 'lala', 'lala@ail.com', '', 'lala'),
+(2, '0', 'lalala', 'lala', 'lala@ail.com', '', 'lala'),
+(3, '0', 'asdas', 'asdas', 'asdas@gmail.asdasd', '', 'asdasdasd'),
+(4, '0', 'qweqweqw', 'qweqweqwe', 'qweqweqwe@asdasd', '', '$2y$10$fF1jMoUTY5f6mnp2riiwvOvrM3y6AXRou'),
+(5, '0', 'reno', 'diandika', 'hermawan@gmail.com', '', '$2y$10$cWQFwD5oHZh3DdG6jYDZJe4kwVnsiUPzs'),
+(6, '0', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$b7n1boRbh2V7y0TFen/3FODV0GxBjTCk3'),
+(7, '0', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$EMElGfOFqx4xFeVCf1nJ9.H28ePNqhQYb'),
+(8, '0', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$Qp6xNb.m5XLhBjfw53rYUevcEBFPdDvAw'),
+(9, '0', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$3cvn8ExnVGZwVUzbXTBmfuZrn182tmQIk'),
+(10, '0', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$Ek/c5c.OCTzIUuSmBNFgau3GFHhOwk5zW'),
+(11, '', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$Ip0E3tT14k.lBOC7NhNVyu6L8UXPJa84L'),
+(12, '', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$fAsAdaRUDtVW6WJdjuNTeOvl2hPPLnOKh'),
+(13, '', 'renooo', 'rennnoo', 'eqweqwe@gmail.com', 'Male', '$2y$10$EEQJyU8EJxKD4m5a.bzUOO1ZQgvCfXIyV'),
+(14, '545646845', 'reno diandika hermawan', 'renoo', 'reno@reno.com', 'Male', '$2y$10$BhscWIGys8GXNGZQYWN8BeHDdLNoPpLyA'),
+(15, '545646845', 'reno diandika hermawan', 'renoo', 'reno@reno.com', 'Male', '$2y$10$EPCJ2DBuN3Z0zN2Ea1I93utvhrOFwQDO6'),
+(16, '545646845', 'reno diandika hermawan', 'renoo', 'reno@reno.com', 'Male', '$2y$10$uCUuPT1jCrsBQMI1/m1woOnoM053kdpCn'),
+(17, '545646845', 'reno diandika hermawan', 'renoo', 'reno@reno.com', 'Male', '$2y$10$2bFvZDWn6oeshWS/Fw3oPulmS7Y9kitaJ'),
+(18, '545646845', 'reno diandika hermawan', 'renoo', 'reno@reno.com', 'Male', '$2y$10$TCz2xeT9wmWL4EWZsNAoIepXpcdm1PkCD');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +80,7 @@ ALTER TABLE `t_user`
 -- AUTO_INCREMENT for table `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `userID` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+  MODIFY `userID` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
